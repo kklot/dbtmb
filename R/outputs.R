9d6a0a04b3264de762ceee58def2fd8f9ca27836
Kinh Nguyen <fieeuvaan@gmail.com>
2020-10-23 00:54:03 +0200

    output

--- R/outputs.R
+++ R/outputs.R
@@ -1,11 +1,10 @@
 #'@export 
 as_array <- function(fit, ...) UseMethod("as_array")
+#'@export 
 as_array.dbtmb <- function(fit, name='median') {
   rp <- fit$obj$report()
   o <- array(rp[[name]], rp$rdims)
-  dimnames(o)[[1]] <- sort(unique(all_cb$age))
-  dimnames(o)[[2]] <- sort(unique(all_cb$yob))
-  dimnames(o)[[3]] <- rownames(readRDS('data/nbmat_SSA.rds'))
+  dimnames(o) <- with(fit$meta, list(age=age, yob=yob, cc=cc_id$ISO_A3))
   o
 }
 #'@export 
@@ -18,7 +17,7 @@ predict <- function(fit, ...) UseMethod("predict")
 #' @param par parameter set to simulate
 #' @param cutoff vector, can be length one
 #' @export
-predict.dbtmb <- function(fit, par, type=c("eversex"), cutoff=24, long=TRUE,...) {
+predict.dbtmb <- function(fit, par, type=c("eversex"), cutoff=24, long=TRUE, quick=FALSE,...) {
   if (missing(par)) par <- fit$obj$env$last.par
   r <- fit$obj$report(par) # can check this and not redoing env$reportenv$...
   scale <- array(r$lambda, r$rdims)
@@ -26,10 +25,9 @@ predict.dbtmb <- function(fit, par, type=c("eversex"), cutoff=24, long=TRUE,...)
   skew <- sweep(array(0, r$rdims), 3, r$a_vec, '+')
   o <- sapply(cutoff, function(x) mapply(F_gllogisI, x, scale, shape, skew))
   dim(o) <- c(r$rdims, length(cutoff))
-  dimnames(o)[[1]] <- sort(unique(all_cb$age))
-  dimnames(o)[[2]] <- sort(unique(all_cb$yob))
-  dimnames(o)[[3]] <- rownames(readRDS('data/nbmat_SSA.rds'))
-  dimnames(o)[[4]] <- cutoff
+  dimnames(o) <- with(fit$meta, list(age=age, yob=yob, cc=cc_id$ISO_A3, kut=cutoff))
+  if (quick) 
+    return(o)
   if (long) {
     o <- as.data.table(o)
     setnames(o, colnames(o), c('age', 'yob', 'ISO_A3', 'kut', type))
